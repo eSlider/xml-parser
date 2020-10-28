94951ac72b6bfaef474875ee822f5875e34e0808
Andriy Oblivantsev <eslider@gmail.com>
2020-10-28 10:22:27 +0100

    Improve BaseEntity

--- Entity/BaseEntity.php
+++ Entity/BaseEntity.php
@@ -1,29 +1,22 @@
 <?php
+
 namespace Eslider\XML\Entity;
 
-/**
- * Class BaseEntity
- *
- * @author  Andriy Oblivantsev <eslider@gmail.com>
- */
-class BaseEntity
-{
-    protected $_data;
-    protected $saveOriginalData;
+use ReflectionClass;
+use ReflectionException;
+
+class BaseEntity {
+    private ReflectionClass $ref;
 
     /**
      * BaseEntity constructor.
      *
-     * @param array $data
-     * @param bool  $saveOriginalData Save testing friendly original data as array?.
+     * @param array|null $data
+     * @throws ReflectionException
      */
-    public function __construct(array &$data = null, $saveOriginalData = false)
+    public function __construct(array $data = null)
     {
-        $this->saveOriginalData = $saveOriginalData;
         if ($data) {
-            if ($saveOriginalData) {
-                $this->_data = $data;
-            }
             if (isset($data['@attributes'])) {
                 $this->fill($data['@attributes']);
             }
@@ -31,47 +24,50 @@ class BaseEntity
         }
     }
 
+    protected function toUpperCase($matches)
+    {
+        return strtoupper($matches[1]);
+    }
+
     /**
      * @param array $data
+     * @throws ReflectionException
      */
     protected function fill(array &$data)
     {
         static $className, $methods, $vars, $reflection;
-
         if (!$className) {
-            $className  = get_class($this);
-            $methods    = get_class_methods($className);
-            $vars       = array_keys(get_class_vars($className));
-            $reflection = new \ReflectionClass($className);
+            $className = get_class($this);
+            $methods = get_class_methods($className);
+            $vars = array_keys(get_class_vars($className));
+            $reflection = $this->ref = new ReflectionClass($className);
         }
-
         foreach ($data as $k => $v) {
             if ($k == "@attributes") {
                 continue;
             }
 
-            $methodName = 'set' . ucfirst($k);
+            $methodName = 'set'.ucfirst($k);
             if (in_array($methodName, $methods)) {
                 $this->{$methodName}($v);
                 continue;
             }
 
-            $methodName = 'set' . ucfirst($this->removeNameSpaceFromVariableName($k));
+            $methodName = 'set'.ucfirst($this->removeNameSpaceFromVariableName($k));
             if (in_array($methodName, $methods)) {
                 $this->{$methodName}($v);
+                continue;
+            }
+
+            $varName = preg_replace_callback("/[_-](\w)/", fn($matches) => strtoupper($matches[1]), $k);
+            if (in_array($varName, $vars)) {
+                $this->setProperty($varName, $v);
                 continue;
             }
 
             $varName = lcfirst($this->removeNameSpaceFromVariableName($k));
             if (in_array($varName, $vars)) {
-                $docComment = $reflection->getProperty($varName)->getDocComment();
-                if (preg_match('/@var ([\\\]?[A-Z]\S+)/s', $docComment, $annotations)) {
-                    $varClassName = $annotations[1];
-                    if (class_exists($varClassName)) {
-                        $v = new $varClassName($v, $this->saveOriginalData);
-                    }
-                }
-                $this->{$varName} = $v;
+                $this->setProperty($varName, $v);
                 continue;
             }
 
@@ -81,11 +77,11 @@ class BaseEntity
                 if ($annotations = self::parse('/@var\s+([\\\]?[A-Z]\S+)(\[\])/s', $docComment)) {
                     $varClassName = $annotations[1];
                     if (class_exists($varClassName)) {
-                        $items     = array();
+                        $items = array();
                         $isNumeric = is_int(key($v));
-                        $list      = $isNumeric ? $v : array($v);
+                        $list = $isNumeric ? $v : array($v);
                         foreach ($list as $subData) {
-                            $items[] = new $varClassName($subData, $this->saveOriginalData);
+                            $items[] = new $varClassName($subData);
                         }
                         $v = $items;
                     }
@@ -105,16 +101,35 @@ class BaseEntity
         return preg_replace("/^.+?_/", '', $name);
     }
 
-
     /**
      * @param $reg
      * @param $str
      * @return null
      */
-    public static function parse($reg, $str)
+    private static function parse($reg, $str)
     {
         $annotations = null;
         preg_match($reg, $str, $annotations);
+
         return $annotations;
+    }
+
+    /**
+     * @param string $name Property name
+     * @param mixed $value value
+     * @throws ReflectionException
+     */
+    protected function setProperty(string $name, $value)
+    {
+        $docComment = $this->ref->getProperty($name)->getDocComment();
+        // Try to get type and create a instance
+        if (preg_match('/@var ([\\\]?[A-Z]\S+)/s', $docComment, $annotations)) {
+            $varClassName = $annotations[1];
+            if (class_exists($varClassName)) {
+                $this->{$name} = new $varClassName($value);
+            }
+        } else {
+            $this->{$name} = $value;
+        }
     }
 }
